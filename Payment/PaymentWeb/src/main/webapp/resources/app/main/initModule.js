e69820439db2da2f7139230b3359f5918439f076
Shankar kota <shankar.kota@aa.com>
2017-02-11 16:58:47 +0530

    Added  angular messages

--- Payment/PaymentWeb/src/main/webapp/resources/app/main/initModule.js
+++ Payment/PaymentWeb/src/main/webapp/resources/app/main/initModule.js
@@ -1,32 +1,33 @@
 import angular from 'angular';
-import bootstrap from 'angular-ui-bootstrap';
 import angularAria from 'angular-aria';
 import angularAnimate from 'angular-animate';
 import angularMaterial from 'angular-material';
+import angularCookies from 'angular-cookies';
+import angularMessages from 'angular-messages';
+import bootstrap from 'angular-ui-bootstrap';
 import tabs from 'angular-ui-bootstrap/src/tabs';
 import accordion from 'angular-ui-bootstrap/src/accordion';
 import datepicker from 'angular-ui-bootstrap/src/datepicker';
 import timepicker from 'angular-ui-bootstrap/src/timepicker';
 import modal from 'angular-ui-bootstrap/src/modal';
 import uiRouter from 'angular-ui-router';
-import angularCookies from 'angular-cookies';
 
 angular.module("payment", [
-	bootstrap, 
+	bootstrap,
 	angularAria,
 	angularAnimate,
 	angularMaterial,
+	angularCookies,
+	angularMessages,
 	tabs,
 	uiRouter,
 	accordion,
 	datepicker,
 	timepicker,
-	modal,	
-	angularCookies,
-	'jcs-autoValidate'
+	modal
 ]);
 
 setTimeout(
-  function asyncBootstrap() {
-     angular.bootstrap( document, [ "payment" ] );
-},( 2 * 1000 ));
+	function asyncBootstrap() {
+		angular.bootstrap(document, ["payment"]);
+	}, (2 * 1000));
