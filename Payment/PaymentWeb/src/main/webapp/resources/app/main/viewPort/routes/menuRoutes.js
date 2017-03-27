720d5a9e35c1e0d72962847ae963fe40db76a26e
Shankar kota <shankar.kota@aa.com>
2017-03-27 16:21:42 +0530

    make resource path as constant

--- Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/routes/menuRoutes.js
+++ Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/routes/menuRoutes.js
@@ -3,33 +3,35 @@
 
     function menuRoutes($stateProvider, $urlRouterProvider) {
 
+        const prefixUrl = 'javax.faces.resource/';
+
         $urlRouterProvider.otherwise("/billing");
 
         $stateProvider.state('home', {
             url: '/',
         }).state('billing', {
             url: '/billing',
-            templateUrl: 'javax.faces.resource/app/main/viewPort/content/billing/billing.template.html',
+            templateUrl: prefixUrl + 'app/main/viewPort/content/billing/billing.template.html',
             controller: 'billingController',
             controllerAs: 'billingController'
         }).state('viewBills', {
             url: '/viewBills',
-            templateUrl: 'javax.faces.resource/app/main/viewPort/content/viewBills/viewBills.template.html',
+            templateUrl: prefixUrl + 'app/main/viewPort/content/viewBills/viewBills.template.html',
             controller: 'viewBillsController',
             controllerAs: 'viewBillsController'
         }).state('manage', {
             url: '/manage',
-            templateUrl: 'javax.faces.resource/app/main/viewPort/content/manage/manage.template.html',
+            templateUrl: prefixUrl + 'app/main/viewPort/content/manage/manage.template.html',
             controller: 'manageController',
             controllerAs: 'manageController'
         }).state('manage.items', {
             url: '/items',
-            templateUrl: 'javax.faces.resource/app/main/viewPort/content/viewBills/viewBills.template.html',
+            templateUrl: prefixUrl + 'app/main/viewPort/content/viewBills/viewBills.template.html',
             controller: 'itemsController',
             controllerAs: 'itemsController'
         }).state('manage.companies', {
             url: '/companies',
-            templateUrl: 'javax.faces.resource/app/main/viewPort/content/viewBills/viewBills.template.html',
+            templateUrl: prefixUrl + 'app/main/viewPort/content/viewBills/viewBills.template.html',
             controller: 'itemsController',
             controllerAs: 'itemsController'
         });
