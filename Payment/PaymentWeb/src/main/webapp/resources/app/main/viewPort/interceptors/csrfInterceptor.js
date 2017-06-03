b5d2510aeb409652d95f867dc9f38d7f0a54bf00
Shankar kota <shankar.kota@aa.com>
2017-06-03 16:31:33 +0530

    if token not in cookie then redirect to login

--- Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/interceptors/csrfInterceptor.js
+++ Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/interceptors/csrfInterceptor.js
@@ -4,15 +4,27 @@
 
     function csrfInterceptor($cookies, $injector) {
 
+        /**
+         * Function to show session expired popup.
+         */
+        function sessionExpired(title) {
+            let userActivityService = $injector.get("userActivityService");
+            let utilsService = $injector.get("utilsService");
+            utilsService.alertPopup(title, "Please Login...", userActivityService.navigateTologin);
+            return null;
+        }
+
         return {
 
             request: function (config) {
 
                 var token = $cookies.get('XSRF-TOKEN');
-                console.log("all cookies: ");
-                console.log($cookies.getAll());
+                //console.log("all cookies: ");
+                // console.log($cookies.getAll());
                 if (token) {
                     config.headers['X-CSRF-TOKEN'] = token;
+                } else {
+                    sessionExpired("Unable to authunticate you!!");
                 }
                 return config;
             },
@@ -20,14 +32,11 @@
             response: function (responseConfig) {
 
                 if (responseConfig.config.url.indexOf("/logout") == -1 && typeof responseConfig.data === 'string' && responseConfig.data.indexOf("<html>") != -1) {
-                    let userActivityService = $injector.get("userActivityService");
-                    let utilsService = $injector.get("utilsService");
-                    utilsService.alertPopup("Session Expired!!", "Please Login...", userActivityService.navigateTologin);
-                    return null;
+                    sessionExpired("Session Expired!!");
                 }
                 return responseConfig;
             }
-        }
+        };
 
     }
 
