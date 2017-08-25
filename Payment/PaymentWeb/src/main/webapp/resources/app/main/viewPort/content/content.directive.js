7ad598f2ff6036265b1f36208d5c5b77da09ed3b
Shankar kota <shankar.kota@aa.com>
2017-08-25 17:37:43 +0530

    Make custom directives as restricted to Element

--- Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/content/content.directive.js
+++ Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/content/content.directive.js
@@ -3,11 +3,12 @@
 
     function content() {
         return {
+            restrict: 'E', // Resttrict to Element
             template: require('./content.template.html'),
             controller: 'contentController',
             controllerAs: 'contentController'
         };
     };
 
-    angular.module('payment').directive('content', content);
+    angular.module('payment').directive('paymentContent', content);
 };
--- Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/menu/menu.directive.js
+++ Payment/PaymentWeb/src/main/webapp/resources/app/main/viewPort/menu/menu.directive.js
@@ -3,12 +3,13 @@
 
     function menu() {
         return {
+            restrict: 'E',
             template: require('./menu.template.html'),
             controller: 'menuController',
             controllerAs: 'menuController'
         }
     };
 
-    angular.module("payment").directive('menu', menu);
+    angular.module("payment").directive('paymentMenu', menu);
 
 };
